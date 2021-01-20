586f1a43f19e3f7f17e11f5223f3c280f0f51c66
Sergio Muñoz <59663486+selcox@users.noreply.github.com>
2021-01-20 22:34:23 +0100

    Added resize support

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import * as PIXI from "pixi.js";
 
 const app = new PIXI.Application({
-    width: 256,
-    height: 256,
+    width: window.innerWidth,
+    height: window.innerHeight,
     antialias: true
+});
+
+window.addEventListener('resize', (event) => {
+    app.renderer.resize(window.innerWidth, window.innerHeight);
 });
 
 app.renderer.backgroundColor = 0x061639;
